48a90acaafd2d13971a2fc9ce977958bfe1dc883
Bobreogen <goldakkordeon@mail.ru>
2020-07-09 17:52:34 +0700

    first build

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module awesomeProject
 go 1.14
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.7.5-0.20200517040254-948bec34b516
-	go.mongodb.org/mongo-driver v1.3.4 // indirect
+	go.mongodb.org/mongo-driver v1.3.4
+	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5
 )
--- main.go
+++ main.go
@@ -2,51 +2,229 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+type User struct {
+	ID primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	GUID string `json:"guid"`
+	Refresh string `json:"refresh"`
+	Access string `json:"access"`
+
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	r := mux.NewRouter()
 
-	// Страница по умолчанию для нашего сайта это простой html.
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
 
-	// Статику (картинки, скрипти, стили) будем раздавать
-	// по определенному роуту /static/{file}
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
-		http.FileServer(http.Dir("./static/"))))
-	r.Handle("/get-tokens", NotImplemented).Methods("GET")
-	r.Handle("/refresh", NotImplemented).Methods("POST")
-	r.Handle("/delete-token", NotImplemented).Methods("DELETE")
-	r.Handle("/delete-tokens", NotImplemented).Methods("DELETE")
+	r.Handle("/get-tokens", get).Methods("GET")
+	r.Handle("/refresh", refr).Methods("POST")
+	r.Handle("/delete-token", delR).Methods("DELETE")
+	r.Handle("/delete-tokens", delGUID).Methods("DELETE")
 
-	http.ListenAndServe(":" + port, r)
+	_ = http.ListenAndServe(":"+port, r)
 
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:adminadmin@bobreogen.vmkfp.mongodb.net/admin?retryWrites=true&w=majority"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Connect(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-	//
-	//err = client.Ping(context.TODO(), nil)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//fmt.Println("Connected to MongoDB!")
-	//_ = client.Database("Admin").Collection("users")
 }
 
-var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Not Implemented"))
+var get = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	if r.FormValue("GUID") != "" {
+
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		acStr, reStr := getAR()
+		collection := getCollection()
+		defer func() { _ = collection.Database().Client().Disconnect(ctx) }()
+
+		hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(reStr), bcrypt.DefaultCost)
+
+		callback := func(sessCtx mongo.SessionContext) (interface{}, error) {
+
+			filter := bson.M{"guid": r.FormValue("GUID")}
+			_, _ = collection.DeleteMany(context.TODO(), filter)
+			result := User{primitive.NewObjectID(), r.FormValue("GUID"), base64.StdEncoding.EncodeToString(hashedPassword), acStr}
+			_, _ = collection.InsertOne(context.TODO(), result)
+
+
+			return nil, nil
+		}
+		session, err := collection.Database().Client().StartSession()
+		if err != nil {
+			panic(err)
+		}
+		defer session.EndSession(ctx)
+
+		_, err = session.WithTransaction(ctx, callback)
+		if err != nil {
+			panic(err)
+		}
+		_, _ = w.Write([]byte("GUID: " + r.FormValue("GUID") + "\naccess token: " + acStr + "\nrefresh token: " + reStr))
+	} else {
+		_, _ = w.Write([]byte("Don't get a GUID"))
+	}
 })
+
+var refr = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	if r.FormValue("refresh") != ""{
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		collection := getCollection()
+		defer func() { _ = collection.Database().Client().Disconnect(ctx) }()
+
+		callback := func(sessCtx mongo.SessionContext) (interface{}, error) {
+
+			var res []User
+			cursor, err := collection.Find(ctx, bson.M{})
+			if err != nil {
+				fmt.Println(err)
+			}
+			for cursor.Next(ctx) {
+				var n User
+				_ = cursor.Decode(&n)
+				res = append(res, n)
+			}
+			bl := false
+			for _, el := range res {
+				dRefr, _ := base64.StdEncoding.DecodeString(el.Refresh)
+				err = bcrypt.CompareHashAndPassword( dRefr , []byte(r.FormValue("refresh")))
+				if err == nil {
+					_, _ = collection.DeleteOne(ctx, bson.M{"refresh": el.Refresh})
+					acStr, reStr := getAR()
+					hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(reStr), bcrypt.DefaultCost)
+					result := User{primitive.NewObjectID(), el.GUID, base64.StdEncoding.EncodeToString(hashedPassword), acStr}
+					_, _ = collection.InsertOne(context.TODO(), result)
+					_, _ = w.Write([]byte("access token: " + acStr + "\nrefresh token: " + reStr))
+					bl = true
+				}
+			}
+			if !bl {
+				_, _ = w.Write([]byte("Don't find valid refresh token"))
+			}
+			return nil, nil
+		}
+		session, err := collection.Database().Client().StartSession()
+		if err != nil {
+			panic(err)
+		}
+		defer session.EndSession(ctx)
+
+		_, err = session.WithTransaction(ctx, callback)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		_, _ = w.Write([]byte("Don't get valid refresh token"))
+	}
+})
+
+var delR = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	if r.FormValue("refresh") != ""{
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		collection := getCollection()
+		defer func() { _ = collection.Database().Client().Disconnect(ctx) }()
+
+		callback := func(sessCtx mongo.SessionContext) (interface{}, error) {
+
+			var res []User
+			cursor, err := collection.Find(ctx, bson.M{})
+			if err != nil {
+				fmt.Println(err)
+			}
+			for cursor.Next(ctx) {
+				var n User
+				_ = cursor.Decode(&n)
+				res = append(res, n)
+			}
+			bl := false
+			for _, el := range res {
+				dRefr, _ := base64.StdEncoding.DecodeString(el.Refresh)
+				err = bcrypt.CompareHashAndPassword( dRefr , []byte(r.FormValue("refresh")))
+				if err == nil {
+					_, _ = collection.DeleteOne(ctx, bson.M{"refresh": el.Refresh})
+					_, _ = w.Write([]byte("Delete complete"))
+					bl = true
+				}
+			}
+			if !bl {
+				_, _ = w.Write([]byte("Don't find valid refresh token"))
+			}
+			return nil, nil
+		}
+		session, err := collection.Database().Client().StartSession()
+		if err != nil {
+			panic(err)
+		}
+		defer session.EndSession(ctx)
+
+		_, err = session.WithTransaction(ctx, callback)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		_, _ = w.Write([]byte("Don't get valid refresh token"))
+	}
+})
+
+var delGUID = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("GUID") != "" {
+
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		collection := getCollection()
+		defer func() { _ = collection.Database().Client().Disconnect(ctx) }()
+
+		callback := func(sessCtx mongo.SessionContext) (interface{}, error) {
+
+			filter := bson.M{"guid": r.FormValue("GUID")}
+			_, _ = collection.DeleteMany(context.TODO(), filter)
+
+			_, _ = w.Write([]byte("Delete complete"))
+			return nil, nil
+		}
+		session, err := collection.Database().Client().StartSession()
+		if err != nil {
+			panic(err)
+		}
+		defer session.EndSession(ctx)
+
+		_, err = session.WithTransaction(ctx, callback)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		_, _ = w.Write([]byte("Don't get a GUID"))
+	}
+})
+
+func getAR() (acStr string, reStr string){
+	atClaims := jwt.MapClaims{}
+	atClaims["exp"] = time.Now().Add(time.Hour * 240).Unix()
+	access := jwt.New(jwt.SigningMethodHS512)
+	acStr, err := access.SignedString([]byte("secret" + string(time.Now().Nanosecond())))
+	if err != nil {
+		log.Fatal(err)
+	}
+	refresh := jwt.NewWithClaims(jwt.SigningMethodHS512, atClaims)
+	reStr, err = refresh.SignedString([]byte(string(time.Now().Nanosecond()) + "secret"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+func getCollection() *mongo.Collection {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://user:useruser@bobreogen.vmkfp.mongodb.net/admin?retryWrites=true&w=majority"))
+	collection := client.Database("Users").Collection("users")
+	return collection
+}
